4cf1f3ff3887846f10c75d97d119c0c5c4db8d17
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-08-24 19:40:17 -0600

    Create method to test Cell class initialization and Cell class method

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,6 +2,8 @@ require './lib/ship'
 
 class Cell
 
-  
+  def initialize(coordinate)
+    @coordinate = coordinate
+  end
 
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -5,6 +5,10 @@ require './lib/cell'
 
 class CellTest < Minitest:: Test
 
+  def test_it_exists
+    cell = Cell.new("B4")
 
+    assert_instance_of Cell, cell
+  end
 
 end
