e1149dcadd5d3241306b8e7007fa0aadd2d2c629
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-25 17:21:26 -0600

    Add generate_cards method to board

--- lib/board.rb
+++ lib/board.rb
@@ -9,5 +9,11 @@ class Board
   end
 
   def generate_cells
+    cell_hash = {}
+    coordinates = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    coordinates.each do |coordinate|
+      cell_hash[coordinate] = Cell.new(coordinate)
+    end
+    cell_hash
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -9,4 +9,12 @@ class BoardTest < Minitest::Test
 
     assert_instance_of Board, board
   end
+
+  def test_it_holds_cells
+    board = Board.new
+
+    assert_equal 16, board.cells.keys.count
+    assert_equal 16, board.cells.values.count
+    assert_instance_of Cell, board.cells["D4"]
+  end
 end
