c89435e9b161b548b3bf6cbc06582c89ccc3a3d5
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-08-26 18:02:41 -0600

    Create a method to test diagonal coordinates invalid and valid coordinates true

--- test/board_test.rb
+++ test/board_test.rb
@@ -45,7 +45,7 @@ class BoardTest < Minitest::Test
     assert board.valid_placement?(cruiser, ["A1", "A2", "A3"])
   end
 
-  def test_it_can_validate_consecutive_coordinates
+  def test_nonconsecutive_coordinates_are_invalid
     board = Board.new
 
     cruiser = Ship.new("Cruiser", 3)
@@ -55,7 +55,24 @@ class BoardTest < Minitest::Test
     refute board.valid_placement?(submarine, ["A1", "C1"])
     refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
     refute board.valid_placement?(submarine, ["C1", "B1"])
+  end
 
+  def test_diagonal_coordinates_are_invalid
+    board = Board.new
+
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    refute board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    refute board.valid_placement?(submarine, ["C2", "D3"])
+  end
+
+  def test_valid_coordinates
+    board = Board.new
+
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    
     assert board.valid_placement?(submarine, ["C1", "C2"])
     assert board.valid_placement?(submarine, ["B1", "C1"])
     assert board.valid_placement?(cruiser, ["A1", "B1", "C1"])
@@ -63,6 +80,4 @@ class BoardTest < Minitest::Test
     assert board.valid_placement?(submarine, ["A1", "A2"])
     assert board.valid_placement?(cruiser, ["B1", "C1", "D1"])
   end
-
-  
 end
