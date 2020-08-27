5f29e95ad60891f241f2733045fa8abe5620ce97
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-27 16:18:46 -0600

    Add assertions to Game_Test for Game attributes

--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,7 @@ require "./lib/board"
 require "./lib/player"
 
 class Game
+  attr_reader :player1, :player2, :winner
 
   def initialize(player1, player2)
     @player1 = player1
--- test/game_test.rb
+++ test/game_test.rb
@@ -5,9 +5,20 @@ require "./lib/game"
 class GameTest < Minitest::Test
 
   def test_it_is_game
-    player1 = Player.new()
+    player1 = Player.new
+    player2 = Player.new
     game = Game.new(player1, player2)
 
     assert_instance_of Game, game
+  end
+
+  def test_it_has_attributes
+    player1 = Player.new
+    player2 = Player.new
+    game = Game.new(player1, player2)
+
+    assert_instance_of Player, player1
+    assert_instance_of Player, player2
+    assert_nil game.winner
   end
 end
