6007311ec914cd680ad0e652ff820566237de7e2
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-28 13:23:10 -0600

    Add Game introduction and player name methods

--- lib/game.rb
+++ lib/game.rb
@@ -11,4 +11,22 @@ class Game
     @cpu = Player.new
     @winner = nil
   end
+
+  def introduction
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+    input = gets.chomp
+    if input == "p"
+      set_player_name
+    elsif input == "q"
+      abort("We'll see you next time!")
+    end
+  end
+
+  def set_player_name
+    player.name = nil
+    puts "What is your name? You may leave this blank to not be named."
+    input = gets.chomp
+    player.name = input if input != ""
+  end
 end
