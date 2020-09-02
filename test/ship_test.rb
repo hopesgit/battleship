4d896635f3d29eac303176edf506b17fe5491fbf
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-02 16:36:02 -0600

    Update Ship_test to use setup method

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,60 +3,49 @@ require 'minitest/pride'
 require './lib/ship'
 
 class ShipTest < Minitest::Test
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
 
   def test_it_exists
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_instance_of Ship, cruiser
+    assert_instance_of Ship, @cruiser
   end
 
   def test_it_has_a_name
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal "Cruiser", cruiser.name
+    assert_equal "Cruiser", @cruiser.name
   end
 
   def test_it_has_a_length
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal 3, cruiser.length
+    assert_equal 3, @cruiser.length
   end
 
   def test_it_has_health
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal 3, cruiser.health
+    assert_equal 3, @cruiser.health
   end
 
   def test_it_begins_as_afloat
-    cruiser = Ship.new("Cruiser", 3)
-
-    refute cruiser.sunk?
+    refute @cruiser.sunk?
   end
 
   def test_it_can_get_hit
-    cruiser = Ship.new("Cruiser", 3)
+    assert_equal 3, @cruiser.health
 
-    assert_equal 3, cruiser.health
+    @cruiser.hit
 
-    cruiser.hit
-
-    assert_equal 2, cruiser.health
+    assert_equal 2, @cruiser.health
   end
 
   def test_it_sinks_when_health_equals_zero
-    cruiser = Ship.new("Cruiser", 3)
+    @cruiser.hit
 
-    cruiser.hit
+    assert_equal false, @cruiser.sunk?
 
-    assert_equal false, cruiser.sunk?
+    @cruiser.hit
 
-    cruiser.hit
+    assert_equal false, @cruiser.sunk?
 
-    assert_equal false, cruiser.sunk?
+    @cruiser.hit
 
-    cruiser.hit
-
-    assert cruiser.sunk?
+    assert @cruiser.sunk?
   end
 end
